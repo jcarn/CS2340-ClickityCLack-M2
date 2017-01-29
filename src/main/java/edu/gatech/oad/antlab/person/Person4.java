ced332f3997df20e07863d76ecfe5fba110f29f8
HenrySaba <henry-saba@hotmail.com>
2017-01-29 18:30:31 -0500

    edit Person4.java

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -31,7 +31,11 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+      String returnString = "";
+        for (int i = 0; i < input.length(); i++) {
+            returnString += input.charAt(i) + 1;
+        }
+        return input;
     }
     
     /**
