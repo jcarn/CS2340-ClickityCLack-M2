584ef59a4b3c054bbb6f2640566f943d880c7155
jcarn <johngmcarnahan@gmail.com>
2017-01-30 02:32:06 +0000

    changed return statement

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -35,7 +35,7 @@ public class Person4 {
         for (int i = 0; i < input.length(); i++) {
             returnString += input.charAt(i) + 1;
         }
-        return input;
+        return returnString;
     }
     
     /**
